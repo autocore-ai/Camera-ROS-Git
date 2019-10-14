f7c952cf2950d070c6a5b6ab8dd76391f9b0d97f
suchang <suchang@autocore.ai>
2019-10-14 10:09:24 +0800

    support multi models

--- yolo_lib/yolo_helper.cpp
+++ yolo_lib/yolo_helper.cpp
@@ -352,7 +352,7 @@ void YoloHelper::postProcess(DPUTask *task, Mat &frame, int sWidth, int sHeight)
     
     cv::imshow("detect result", frame);
     cv::waitKey(30);
-    cv::imwrite("/root/sc/detect_result.jpg",frame);
+    //cv::imwrite("/root/detect_result.jpg",frame);
     //cout<<"postProcess end "<<endl;
 }
 
